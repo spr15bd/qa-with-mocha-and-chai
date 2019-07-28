bec480cb5523b4242b5a5615cbf3d8ef9bd9ecc9
Glitch (raspy-sense) <none>
2019-07-28 15:52:11 +0000

    Import of freeCodeCamp/boilerplate-mochachai

--- public/client.js
+++ public/client.js
@@ -1,42 +1,104 @@
-// client-side js
-// run by the browser each time your view template is loaded
+function Utils() {
+   this.ready = function (fn) {
+      if (typeof fn !== 'function') {
+         return;
+      }
 
-console.log('hello world :o');
+      if (document.readyState === 'complete') {
+         return fn();
+      }
 
-// our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-];
+      document.addEventListener('DOMContentLoaded', fn, false);
+   };
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
+   this.ajax = function (options, cb) {
+      var xmlhttp = new XMLHttpRequest();
 
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
+      xmlhttp.onreadystatechange = function () {
+         if (xmlhttp.readyState === 4){
+           if(Math.floor(xmlhttp.status/100) === 2) {
+             var results = xmlhttp.responseText;
+             var type = xmlhttp.getResponseHeader('Content-Type');
+             if(type.match('application/json')) {
+               results = JSON.parse(results);
+             }
+             cb(null, results);
+           } else {
+             cb(xmlhttp);
+           }
+         }
+      };
+
+      var method = options.method || 'get';
+      var url = options.url || '/';
+
+      if(url.charAt(url.length - 1) === '/')
+        url = url.slice(0,url.length-1);
+
+      if (options.data) {
+        var query;
+        var contentType = "application/x-www-form-urlencoded";
+        if(options.type && options.type === 'json') {
+          query = JSON.stringify(options.data);
+          contentType = "application/json";
+        } else {
+          query = [];
+          for (var key in params) {
+            query.push(key + '=' + encodeURIComponent(params[key]));
+            query.push('&');
+          }
+          query.pop();
+          query = query.join('');
+        }
+
+        switch(method.toLowerCase()){
+          case 'get':
+            url += ('?' + query);
+            xmlhttp.open(method, url, true);
+            xmlhttp.send();
+            break;
+          case 'put':
+          case 'patch':
+          case 'delete':
+          case 'post':
+            xmlhttp.open(method, url, true);
+            xmlhttp.setRequestHeader("Content-type", contentType);
+            xmlhttp.send(query);
+            break;
+          default:
+            return;
+        }
+      } else {
+        xmlhttp.open(method, url, true);
+        xmlhttp.send();
+      }
+
+   };
 }
 
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
+var utils = new Utils();
+
+utils.ready(function() {
+
+  var form = document.getElementById('f1');
+  var input = document.getElementById('i1');
+  var div = document.getElementById('tn');
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(input.value) {
+      var options = {
+        method: 'put',
+        url: '/travellers',
+        type: 'json',
+        data: {surname: input.value}
+      };
+      div.innerHTML = '<p>Loading...</p>';
+      utils.ajax(options, function(err, res) {
+        if(err) return console.log(err);
+        div.innerHTML = '<p>first name: <span id="name">' + res.name + '</span><p>' +
+          '<p>last name: <span id="surname">' + res.surname + '</span><p>' +
+          '<p>dates: <span id="dates">' + res.dates + '</span><p>';
+      });
+    }
+  });
 });
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
-};
